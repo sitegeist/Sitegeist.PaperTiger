106f24d7c1f3d510d8da1a29f73548636c06e9df
Martin Ficzel <ficzel@sitegeist.de>
2023-01-26 16:41:55 +0100

    Initial commit everything here is WIP

---
+++ Classes/FusionObjects/ActionCollectionImplementation.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace Sitegeist\PaperTiger\FusionObjects;
+
+use Neos\Flow\Mvc\ActionResponse;
+use Neos\Fusion\Form\Runtime\Domain\ActionInterface;
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+class ActionCollectionImplementation extends AbstractFusionObject implements ActionInterface
+{
+    protected function getActionMap(): array
+    {
+        $map = $this->fusionValue('actionMap') ?: [];
+        return array_filter(
+            $map,
+            function ($item) {return $item instanceof ActionInterface;}
+        );
+    }
+
+    public function evaluate()
+    {
+        return $this;
+    }
+
+    /**
+     * @return ActionResponse|null
+     */
+    public function perform(): ?ActionResponse
+    {
+        $response = new ActionResponse();
+
+        /**
+         * @var ActionInterface[] $subActions
+         */
+        $subActions = $this->getActionMap();
+
+        foreach ($subActions as $subAction) {
+            $subActionResponse = $subAction->perform();
+            if ($subActionResponse) {
+                // content of multiple responses is concatenated
+                if ($subActionResponse->getContent()) {
+                    $mergedContent = $response->getContent() . $subActionResponse->getContent();
+                    $subActionResponse->setContent($mergedContent);
+                }
+                // preserve non 200 status codes that would otherwise be overwritten
+                if ($response->getStatusCode() !== 200 && $subActionResponse->getStatusCode() == 200) {
+                    $subActionResponse->setStatusCode($response->getStatusCode());
+                }
+                $subActionResponse->mergeIntoParentResponse($response);
+            }
+        }
+        return $response;
+    }
+}
---
+++ Classes/FusionObjects/SchemaCollectionImplementation.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace Sitegeist\PaperTiger\FusionObjects;
+
+
+use Neos\Error\Messages\Result;
+use Neos\Fusion\Form\Runtime\Domain\SchemaInterface;
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+class SchemaCollectionImplementation extends AbstractFusionObject implements SchemaInterface
+{
+    protected function getSchemaMap(): array
+    {
+        $map = $this->fusionValue('schemaMap') ?: [];
+        return array_filter(
+            $map,
+            function ($item) {return $item instanceof SchemaInterface;}
+        );
+    }
+
+    public function evaluate()
+    {
+        return $this;
+    }
+
+    public function validate($data): Result
+    {
+        if (!is_array($data)) {
+            throw new \InvalidArgumentException('The nested schema can only handle arrays');
+        }
+
+        $result = new Result();
+
+        /**
+         * @var SchemaInterface[] $subschemas
+         */
+        $subschemas = $this->getSchemaMap();
+
+        foreach ($subschemas as $fieldName => $fieldSchema) {
+            if ($fieldSchema instanceof SchemaInterface) {
+                $fieldValue = $data[$fieldName] ?? null;
+                $result->forProperty($fieldName)->merge($fieldSchema->validate($fieldValue));
+            }
+        }
+
+        return $result;
+    }
+
+    public function convert($data)
+    {
+        if (!is_array($data)) {
+            throw new \InvalidArgumentException('The nested schema can only handle arrays');
+        }
+        $result = [];
+
+        /**
+         * @var SchemaInterface[] $subschemas
+         */
+        $subschemas = $this->getSchemaMap();
+
+        foreach ($subschemas as $fieldName => $fieldSchema) {
+            if ($fieldSchema instanceof SchemaInterface) {
+                $fieldValue = $data[$fieldName] ?? null;
+                $result[$fieldName] = $fieldSchema->convert($fieldValue);
+            }
+        }
+
+
+        return $result;
+    }
+}
