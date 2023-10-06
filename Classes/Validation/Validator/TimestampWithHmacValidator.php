7dea0d96149e307518e98e6b46a04fc26d032031
Martin Ficzel <ficzel@sitegeist.de>
2023-10-06 14:16:23 +0200

    TASK: Incorporate suggestions from review
    The honeypot field "three" will now be filled via ui event (keyboard,mouse,touch). This may throw off some more robots.
    
    thanks @jonnito

--- Classes/Validation/Validator/TimestampWithHmacValidator.php
+++ Classes/Validation/Validator/TimestampWithHmacValidator.php
@@ -30,11 +30,11 @@ class TimestampWithHmacValidator extends AbstractValidator
         try {
             $timestamp = $this->hashService->validateAndStripHmac($value);
             $age = time() - (int)$timestamp;
-          if ($age > $this->options['maximumAge']) {
-            $this->addError("Timestamp is too old", 1696525723);
-        } else if ($age < $this->options['minimumAge']) {
-            $this->addError("Timestamp is too young", 1696525723);
-        }
+            if ($age > $this->options['maximumAge']) {
+                $this->addError("Timestamp is too old", 1696525723);
+            } else if ($age < $this->options['minimumAge']) {
+                $this->addError("Timestamp is too young", 1696525723);
+            }
         } catch (InvalidArgumentForHashGenerationException | InvalidHashException) {
             $this->addError("Hmac did not match", 1696525723);
         }
