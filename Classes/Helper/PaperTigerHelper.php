2ff9a934811825a31e87a46783821c4335ecc152
Martin Ficzel <ficzel@sitegeist.de>
2025-10-10 14:16:01 +0200

    TASK: Adjust php formatting style

--- Classes/Helper/PaperTigerHelper.php
+++ Classes/Helper/PaperTigerHelper.php
@@ -18,7 +18,12 @@ class PaperTigerHelper implements ProtectedContextAwareInterface
         return $this->hashService->appendHmac((string) time());
     }
 
-    function flattenArray(array $array): array {
+    /**
+     * @param array<int|string, mixed|array<mixed>> $array
+     * @return array<int|string, mixed>
+     */
+    public function flattenArray(array $array): array
+    {
         $result = [];
 
         foreach ($array as $item) {
